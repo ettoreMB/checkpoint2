772380ecae776e1e63ab41886909eacefb5237e9
ettoreMB <ettore.barbosa@gmail.com>
2021-12-08 21:32:04 -0300

    chekpoint3

---
+++ chekpoint3/index.js
@@ -0,0 +1,7 @@
+const cursoModule = require('./modules/cursos');
+
+cursoModule.curso.addAluno('pedro', 22, [10,10,10]);
+
+// cursoModule.curso.checarAprovacao(0);
+
+cursoModule.curso.listaAprovacao()
