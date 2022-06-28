8081f09ab5807e55443df971d2d31caa5d7796b3
Ettore <ettore.barbosa@gmail.com>
2022-06-28 20:41:15 -0300

    login script

--- ettore/scripts/utils/api.js
+++ ettore/scripts/utils/api.js
@@ -6,15 +6,31 @@ export async  function login(email, password) {
     password: password
   }
 
-   let login =  await fetch('https://ctd-todo-api.herokuapp.com/v1/users/login', {
+  try {
+    let login =  await fetch('https://ctd-todo-api.herokuapp.com/v1/users/login', {
       method: 'POST',
       headers: {'content-type': "application/json"},
       body: JSON.stringify(user)
      })
+    let response = await login;
 
+    if( response.status === 200 || response.status === 201) {
+      const jwt = response.jwt
+      sessionStorage.setItem('token', jwt);
 
-    console.log(await login.json())
- 
+      location.href = 'tarefas.html'
+    }
+    
+    if(response.status === 400) {
+      location.href = 'index.html'
+      alert('Email ou senha Invalidos');
+    }
+
+  } catch (error) {
+    location.href = 'index.html'
+    alert(error)
+  }
+
 } 
 
 export async function cadastro(firtsName, lastName,email, password, ) {
@@ -28,7 +44,18 @@ export async function cadastro(firtsName, lastName,email, password, ) {
   const createUserApi = await fetch(`${baseURL}/users`, {
     body: JSON.stringify(newUser)
   })
-
-  console.log(createUserApi.json())
+  
+  createUserApi.json()
 }
 
+export function verificacaoLogin() {
+  onload= () => {
+     alert('Rodou')
+   const token =  sessionStorage.getItem('token')
+    if(!token) {
+    console.log('Não tem token')
+    } else {
+      console.log('Token')
+    }
+  }
+}
