e35da89f277085402dced5f191e5989ab3c191bf
RiosBlack <glinhas55@hotmail.com>
2022-07-06 20:21:39 -0300

    Collor icons

--- ettore/scripts/tasks.js
+++ ettore/scripts/tasks.js
@@ -1,70 +1,68 @@
-import {  createTask, getTasks, getUserInfo, logout } from './utils/api.js';
+import { createTask, getTasks, getUserInfo, logout } from './utils/api.js';
 import { removeSkeletons, renderSkeletons } from './utils/loader.js';
-import { createCompleteTaskDiv,createIncompleteTaskDiv, errorMsg } from './utils/utils.js';
+import {
+    createCompleteTaskDiv,
+    createIncompleteTaskDiv,
+    errorMsg,
+} from './utils/utils.js';
 import { validations } from './utils/validations.js';
 
-const header = document.querySelector('#user-name')
-const task_button =  document.querySelector('#task-button');
+const header = document.querySelector('#user-name');
+const task_button = document.querySelector('#task-button');
 const task_input = document.querySelector('#nova-tarefa');
 const tarefas_pendentes = document.querySelector('.tarefas-pendentes');
 const tarefas_terminadas = document.querySelector('.tarefas-terminadas');
 const logout_button = document.querySelector('#closeApp');
 
-
-
-
 onload = async () => {
-  const token =  sessionStorage.getItem('token')
-    if(!token) {
-      location.href = 'index.html';
+    const token = sessionStorage.getItem('token');
+    if (!token) {
+        location.href = 'index.html';
     } else {
-      renderSkeletons(5,'.tarefas-pendentes');
-      const user = await getUserInfo();
-      header.innerText = user.firstName;
-      setTimeout(await loadTasks(), 200);
-      removeSkeletons('.tarefas-pendentes');
-    };
+        renderSkeletons(5, '.tarefas-pendentes');
+        const user = await getUserInfo();
+        header.innerText = user.firstName;
+        setTimeout(await loadTasks(), 200);
+        removeSkeletons('.tarefas-pendentes');
+    }
 };
 
 async function loadTasks() {
-  const tasks = await getTasks();
-  tasks.map(task => {
-    if(task.completed === true) {
-      createCompleteTaskDiv(tarefas_terminadas,task);
-    } else {
-      createIncompleteTaskDiv(tarefas_pendentes,task);
-    }
-  });
-};
+    const tasks = await getTasks();
+    tasks.map(task => {
+        if (task.completed === true) {
+            createCompleteTaskDiv(tarefas_terminadas, task);
+        } else {
+            createIncompleteTaskDiv(tarefas_pendentes, task);
+        }
+    });
+}
 
-task_button.addEventListener('click',async (e) => {
+task_button.addEventListener('click', async e => {
     e.preventDefault();
     let task_value = validations.removeSpacesFromtext(task_input.value);
-    if(task_value === "") {
-     return  errorMsg('O campo tarefa não pode ser vazio');
-    };
+    if (task_value === '') {
+        return errorMsg('O campo tarefa não pode ser vazio');
+    }
     const new_task = await createTask(task_value);
-    await createIncompleteTaskDiv(tarefas_pendentes,new_task);
+    await createIncompleteTaskDiv(tarefas_pendentes, new_task);
 });
-
 
 logout_button.addEventListener('click', function () {
-
-  Swal.fire({
-    text: "Realmente deseja sair ?",
-    icon: 'question',
-    background: '#8E64C5',
-    color: 'white',
-    showCancelButton: true,
-    confirmButtonColor: 'green',
-    cancelButtonColor: 'red',
-    confirmButtonText: 'Sair',
-    cancelButtonText: 'Cancelar'
-}).then((result) => {
-    if (result.isConfirmed) {
-      logout();
-    }
-})
-  
+    Swal.fire({
+        text: 'Realmente deseja sair ?',
+        iconColor: 'white',
+        icon: 'question',
+        background: '#8E64C5',
+        color: 'white',
+        showCancelButton: true,
+        confirmButtonColor: 'green',
+        cancelButtonColor: 'red',
+        confirmButtonText: 'Sair',
+        cancelButtonText: 'Cancelar',
+    }).then(result => {
+        if (result.isConfirmed) {
+            logout();
+        }
+    });
 });
-
--- ettore/scripts/utils/utils.js
+++ ettore/scripts/utils/utils.js
@@ -1,119 +1,120 @@
-import { deleteTask, updateTask } from "./api.js";
+import { deleteTask, updateTask } from './api.js';
 
 const _DATE_OPTIONS = {
-  year: "2-digit",
-  month: "2-digit",
-  day: "2-digit",
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
 };
 
 function transformDate(date) {
-  const transformedDate = new Intl.DateTimeFormat(
-    "pt-BR",
-    _DATE_OPTIONS
-  ).format(new Date(date));
-  return transformedDate;
+    const transformedDate = new Intl.DateTimeFormat(
+        'pt-BR',
+        _DATE_OPTIONS
+    ).format(new Date(date));
+    return transformedDate;
 }
 
 export function createCompleteTaskDiv(input, task) {
-  const task_li = document.createElement("li");
-  const not_done_div = document.createElement("div");
-  const description_div = document.createElement("p");
-  const name_p = document.createElement("p");
-  const timestamp_p = document.createElement("p");
-  const input_id = document.createElement("input");
-  const buttons_div = document.createElement("div");
-  const delete_button = document.createElement("button");
-  const return_button = document.createElement("button");
-  const return_icon = document.createElement("i");
-  const delete_icon = document.createElement("i");
+    const task_li = document.createElement('li');
+    const not_done_div = document.createElement('div');
+    const description_div = document.createElement('p');
+    const name_p = document.createElement('p');
+    const timestamp_p = document.createElement('p');
+    const input_id = document.createElement('input');
+    const buttons_div = document.createElement('div');
+    const delete_button = document.createElement('button');
+    const return_button = document.createElement('button');
+    const return_icon = document.createElement('i');
+    const delete_icon = document.createElement('i');
 
-  task_li.classList.add("tarefa");
-  not_done_div.classList.add("done");
-  description_div.classList.add("descricao");
-  timestamp_p.classList.add("timestamp");
-  name_p.classList.add("nome");
-  input_id.classList.add("id-number");
-  return_icon.classList.add("fas", "fa-undo-alt", "change");
-  delete_icon.classList.add("far", "fa-trash-alt");
+    task_li.classList.add('tarefa');
+    not_done_div.classList.add('done');
+    description_div.classList.add('descricao');
+    timestamp_p.classList.add('timestamp');
+    name_p.classList.add('nome');
+    input_id.classList.add('id-number');
+    return_icon.classList.add('fas', 'fa-undo-alt', 'change');
+    delete_icon.classList.add('far', 'fa-trash-alt');
 
-  task_li.appendChild(not_done_div);
-  task_li.appendChild(description_div);
+    task_li.appendChild(not_done_div);
+    task_li.appendChild(description_div);
 
-  description_div.appendChild(name_p);
-  description_div.appendChild(timestamp_p);
-  description_div.appendChild(buttons_div);
+    description_div.appendChild(name_p);
+    description_div.appendChild(timestamp_p);
+    description_div.appendChild(buttons_div);
 
-  buttons_div.appendChild(delete_button);
-  buttons_div.appendChild(return_button);
-  delete_button.appendChild(delete_icon);
-  return_button.appendChild(return_icon);
+    buttons_div.appendChild(delete_button);
+    buttons_div.appendChild(return_button);
+    delete_button.appendChild(delete_icon);
+    return_button.appendChild(return_icon);
 
-  input_id.value = task.id;
-  name_p.innerText = task.description;
+    input_id.value = task.id;
+    name_p.innerText = task.description;
 
-  delete_icon.addEventListener("click", function () {
-    Swal.fire({
-      title: "Excluir tarefa?",
-      icon: "question",
-      background: "#8E64C5",
-      color: "white",
-      showCancelButton: true,
-      confirmButtonColor: "green",
-      cancelButtonColor: "red",
-      confirmButtonText: "Sair",
-      cancelButtonText: "Cancelars",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        deleteTask(task.id);
-        location.reload();
-      }
+    delete_icon.addEventListener('click', function () {
+        Swal.fire({
+            title: 'Excluir tarefa?',
+            iconColor: 'white',
+            icon: 'warning',
+            background: '#8E64C5',
+            color: 'white',
+            showCancelButton: true,
+            confirmButtonColor: 'green',
+            cancelButtonColor: 'red',
+            confirmButtonText: 'Sair',
+            cancelButtonText: 'Cancelar',
+        }).then(result => {
+            if (result.isConfirmed) {
+                deleteTask(task.id);
+                location.reload();
+            }
+        });
     });
-  });
 
-  return_icon.addEventListener("click", async () => {
-    await updateTask(task);
-    location.reload();
-  });
+    return_icon.addEventListener('click', async () => {
+        await updateTask(task);
+        location.reload();
+    });
 
-  input.appendChild(task_li);
+    input.appendChild(task_li);
 }
 
 export async function createIncompleteTaskDiv(input, task) {
-  console.log(task);
-  const task_li = document.createElement("li");
-  const not_done_div = document.createElement("div");
-  const description_div = document.createElement("p");
-  const name_p = document.createElement("p");
-  const timestamp_p = document.createElement("p");
+    console.log(task);
+    const task_li = document.createElement('li');
+    const not_done_div = document.createElement('div');
+    const description_div = document.createElement('p');
+    const name_p = document.createElement('p');
+    const timestamp_p = document.createElement('p');
 
-  task_li.classList.add("tarefa");
-  not_done_div.classList.add("not-done");
-  description_div.classList.add("descricao");
-  timestamp_p.classList.add("timestamp");
-  name_p.classList.add("nome");
+    task_li.classList.add('tarefa');
+    not_done_div.classList.add('not-done');
+    description_div.classList.add('descricao');
+    timestamp_p.classList.add('timestamp');
+    name_p.classList.add('nome');
 
-  task_li.appendChild(not_done_div);
-  task_li.appendChild(description_div);
-  description_div.appendChild(name_p);
-  description_div.appendChild(timestamp_p);
+    task_li.appendChild(not_done_div);
+    task_li.appendChild(description_div);
+    description_div.appendChild(name_p);
+    description_div.appendChild(timestamp_p);
 
-  name_p.innerText = task.description;
+    name_p.innerText = task.description;
 
-  not_done_div.addEventListener("click", async (e) => {
-    e.preventDefault();
-    await updateTask(task);
-    location.reload();
-  });
-  console.log(task);
+    not_done_div.addEventListener('click', async e => {
+        e.preventDefault();
+        await updateTask(task);
+        location.reload();
+    });
+    console.log(task);
 
-  timestamp_p.innerText = `Criada em : ${transformDate(task.createdAt)}`;
-  input.appendChild(task_li);
+    timestamp_p.innerText = `Criada em : ${transformDate(task.createdAt)}`;
+    input.appendChild(task_li);
 }
 
 export function errorMsg(message) {
-  const errorMsg = document.createElement("div");
-  const body = document.querySelector("body");
-  body.appendChild(errorMsg);
-  errorMsg.innerText = message;
-  errorMsg.classList.add("messages");
+    const errorMsg = document.createElement('div');
+    const body = document.querySelector('body');
+    body.appendChild(errorMsg);
+    errorMsg.innerText = message;
+    errorMsg.classList.add('messages');
 }
